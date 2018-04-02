3a6cbcaa5ab42edfa7dbc80037fc754c0e3d87ce
Renan Gimenez <renan.gimenez@hotmail.fr>
2018-04-03 01:05:58 +0200

    03/04/2018 Message OK

--- application/Main.java
+++ application/Main.java
@@ -28,6 +28,7 @@ public class Main extends Application {
 			scene = new Scene(root,939,608);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setMinWidth(800);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
